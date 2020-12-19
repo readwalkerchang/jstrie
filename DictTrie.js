42a547d65bc831bd4fe8965b4cdf652f44144fd5
Dusing Chang <dusing@MacBook-Air-van-Dusing.local>
2020-12-19 12:26:17 +0100

    feat: insert method for DictTrie

--- DictTrie.js
+++ DictTrie.js
@@ -57,13 +57,17 @@ var Trie = function(word, prefix, dictionary) {
  * @return {void}
  */
 
+//object[property].push(word);
+//console.log(`${object[property]}`);
+//The for...in statement iterates over all enumerable properties of an object that are keyed 
+//Property accessors provide access to an object's properties by using the dot notation or the bracket notation.
 
 Trie.prototype.insert = function(word) {
   let dict = this.dictionary;
   
   for (const property in dict) {
     if(`${property}`=== word[0]){
-      console.log( `${property}`);
+      dict[property].push(word);
     }
   }
 
--- testDictTrie.js
+++ testDictTrie.js
@@ -1,10 +1,13 @@
 var Trie = require('./DictTrie.js'); //Include Modules
 var trie = new Trie();
-//Testing 1// pass
-/*
+//Testing 3// pass
+
 trie.insert('apple');
+trie.insert('app');
+trie.insert('banana');
+trie.insert('orange');
 console.log(trie.dictionary);
-*/
+
 
 //Testing 2//pass
-trie.insert('apple');
+//trie.insert('apple');
