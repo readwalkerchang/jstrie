d09bf76daf18862c8cb500cbf9d2c99bd33445ba
Dusing Chang <dusing@MacBook-Air-van-Dusing.local>
2020-12-19 10:34:09 +0100

    feat: generate dict-Trie structure

--- Trie_with dictionary.js
+++ Trie_with dictionary.js
@@ -10,16 +10,25 @@ function generateAlphabets() {
   
   //charCodeAt method return the Unicode of the first character in a string
   //fromCharCode method convert a Unicode number into a character
-  
-  
   //implement an object with 26 alphabats key(properties) of empty array
-  let alphabets = generateAlphabets();
-  let dict = new Object();
-  let i ;
-  for(i =0;  i< alphabets.length ;i++ ){
-    dict[alphabets[i]] = [];
+  function generateDict(){
+    let alphabets = generateAlphabets();
+    let dict = new Object();
+    let i ;
+    for(i =0;  i< alphabets.length ;i++ ){
+      dict[alphabets[i]] = [];
+    }
+    return dict;
   }
+
   
-  
-  console.log(dict);
-  
+ /**
+ * Initialize your data structure here.
+ */
+var Trie = function(word, prefix, dictionary) {
+  this.word = word;
+  this.prefix = prefix;
+  this.dictionary = generateDict();
+};
+console.log(generateDict());
+module.exports = Trie; //export modules
